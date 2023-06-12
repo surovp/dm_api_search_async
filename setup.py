d135cd5df420e7eb3a964eef26bb47bc8b18eabd
Pavel Surov <surov_p@tanukitech.ru>
2023-06-12 13:34:28 +0500

    вынес grpc async

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+REQUIRES = []
+
+setup(
+    name='dm_api_search_async',
+    version='0.0.1',
+    packages=['dm_api_search_async'],
+    url='https://github.com/surovp/dm_api_search_async.git',
+    license='MIT',
+    author='Pavel Surov',
+    author_email='-',
+    install_requires=REQUIRES,
+    description='dm api search async'
+)
